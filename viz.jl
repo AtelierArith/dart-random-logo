9c568a2a28a2cd94fef58be0dfb501efb387fc7d
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-08-07 21:21:45 +0900

    Delete Unused file

--- viz.jl
+++
@@ -1,9 +0,0 @@
-using StatsPlots
-using CSV
-using DataFrames
-
-run(`dart compile exe -o run ./bin/dart_random_logo.dart`)
-@time o = read(`./run`, String)
-data = DataFrame(CSV.File(IOBuffer(o), header=["x", "y"]))
-
-(@df data scatter(:x, :y)) |> display
